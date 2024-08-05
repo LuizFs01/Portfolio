ee390a3a99a475dc65ea1b61d96b482ccaa29c48
Luiz Freitas <luiz.cachoeira@edu.unipar.br>
2024-08-05 20:22:23 -0300

    Atualizações mobile-first

--- src/app/components/hero/hero.component.ts
+++ src/app/components/hero/hero.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, Renderer2 } from '@angular/core';
 import { CommonModule } from '@angular/common'; 
 import { ButtonModule } from 'primeng/button'; 
 import { RippleModule } from 'primeng/ripple'; 
@@ -10,8 +10,35 @@ import { RippleModule } from 'primeng/ripple';
   templateUrl: './hero.component.html',
   styleUrls: ['./hero.component.sass'] 
 })
-export class HeroComponent {
+export class HeroComponent implements AfterViewInit {
+
+  constructor(private el: ElementRef, private renderer: Renderer2) {}
+
+  ngAfterViewInit() {
+    const tituloElements = this.el.nativeElement.querySelectorAll('.titulo-principal');
+    tituloElements.forEach((titulo: HTMLElement, index: number) => {
+      this.typeWrite(titulo, index);
+    });
+  }
+
   goToLinkedIn() {
     window.open('https://www.linkedin.com/in/luiz-h-freitas/', '_blank');
   }
+
+  private typeWrite(elemento: HTMLElement, index: number) {
+    const text = elemento.getAttribute('data-text') || elemento.innerText;
+    elemento.innerHTML = ''; // Clear existing content
+  
+    // Ajuste o tempo de atraso conforme necessário para uma escrita mais lenta
+    const typingSpeed = 100; // Tempo em milissegundos por caractere
+    const initialDelay = 500; // Tempo em milissegundos de atraso inicial para o índice do elemento
+  
+    text.split('').forEach((char, i) => {
+      setTimeout(() => {
+        elemento.innerHTML += char;
+      }, typingSpeed * i + (index * initialDelay));
+    });
+  }
+  
+  
 }
